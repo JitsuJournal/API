309d3c22cdcce33c29b29f084256a86ba659efe2
harrisiva <harrishiv6@gmail.com>
2025-06-01 17:00:04 -0400

    Added some comments and setting up scafolding for the main.py's solve function

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 # Fast API entry point
 from fastapi import FastAPI
 
+# Initialize fast APi
 app = FastAPI()
 
+
+# Placeholder endpoint for webservice root
 @app.get('/')
 async def root():
     return{"message": "Hello world"}
+
+
+# TODO:
+# Response model for the jitsujournal friendly
+# directed graph/flowchart
+
+
+
+# Actual endpoint for processing a given user problem
+@app.get('/solve/{problem}')
+def solve(problem: str):
+    """
+    Given a problem faced by the user in their jiu-jitsu practice,
+    return a jitsu-journal friendly directed graph/flowchart.
+    Passed into the app for creating initial nodes and edges.
+    """
+    print(problem)
+    return{"problem": problem}
