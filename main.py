786b78264a2e57482587c6d4503c1b381e911269
harrisiva <harrishiv6@gmail.com>
2025-06-02 13:28:04 -0400

    tested working hypothetical answer generation and response over API

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from typing import Annotated
 # Local
 from src.models import Reactflow
-from src.services.llm import get_gemini
+from src.models.general import Solution
+from src.services.llm import get_gemini, create_paragraph
 # Third party
 from fastapi import FastAPI, Depends
 from google.genai import Client
@@ -23,11 +24,18 @@ def solve(problem: str, client: Annotated[Client, Depends(get_gemini)]):
     return a jitsu-journal friendly directed graph/flowchart.
     Passed into the app for creating initial nodes and edges.
     """
-    print(problem)
-    print(str(client))
 
-    # Generate a hypothetical solution using the user's problem
+    # Create a hypothetical solution paragraph using the users problem
+    hypothetical: Solution = create_paragraph(client, problem).parsed
+
+    # Create embedding using the hypothetical solution
+    # Used for searching tutorials with similar content
+
+
+
     # Retrive similar records to the generated solution from Supabase
+
+
     # Use similar records to ground generated answer (rerank if necessary)
     # Convert grounded answer into steps in a sequence
     # Load techniques into memory for passing as context in next stage
@@ -37,4 +45,4 @@ def solve(problem: str, client: Annotated[Client, Depends(get_gemini)]):
     # NOTE: Handle any errors in the middle, passing msgs w/ appropriate error codes
 
 
-    return{"problem": problem}
+    return{"problem": problem, "response": str(hypothetical.paragraph)}
