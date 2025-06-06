d18b26ff2c120946d43420d5e66bfe0b620dda52
harrisiva <harrishiv6@gmail.com>
2025-06-05 23:12:12 -0400

    Updated the constant nodes and edges to be defined with the general model, and used the shape_nodes and edges function to create them with unique id's, these are then packed into the ReactFlow shape and returned the the user

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # System
 from typing import Annotated
 # Local
-from src.models.reactflow import Reactflow, Node, NodeData, Edge # Last 3 used for test
+from src.models import Reactflow
 from src.models.general import Solution, Sequence, Graph
+from src.models.general import Node, Technique, Edge # Models using for test endpoint
 from src.services.llm import conn_gemini, create_paragraph, create_embedding, ground, extract_sequences, create_flowchart
 from src.services.db import conn_supabase, similarity_search, get_techniques
 from src.utils.general import shape_nodes, shape_edges
@@ -37,7 +38,7 @@ async def root():
     return {"message": "Hello world"}
 
 
-@app.get('/test')
+@app.get('/test', response_model=Reactflow)
 def test():
     """
     Endpoint that returns ReactFlow friendly nodes
@@ -45,63 +46,106 @@ def test():
     multiple times to set positions, redirect,
     or test any other feature in the frontend/client side.
     """
+    # Define constant nodes based on sequence 581
     node_a = Node(
-        id='269f01de-1574-4896-ba3b-9372e07bbc7c', 
-        data=NodeData(
-            technique_id=34,name='Triangle', 
+        id=1,
+        #id='269f01de-1574-4896-ba3b-9372e07bbc7c', 
+        technique=Technique(
+            id=34, name='Triangle', 
+            description="A technique where the legs are used to encircle the opponent's neck and one arm, creating a triangle shape that constricts blood flow to the brain.",
             tags=[{'id': 10, 'name': 'artery'}],
             cat_id=2
         )
     )
     node_b = Node(
-        id='3a2913de-59d6-4ac9-a957-eeb329b7b74d', 
-        data=NodeData(
-            technique_id=9,name='Standard Side Control', 
+        #id='3a2913de-59d6-4ac9-a957-eeb329b7b74d', 
+        id=2,
+        technique=Technique(
+            id=9, name='Standard Side Control',
+            description="Controlling the opponent from the side while pinning their shoulders and hips, maintaining pressure with chest-to-chest contact.",
             tags=[{'id': 2, 'name': 'seated'},{'id': 1, 'name': 'top'}],
             cat_id=1
         )
     )
     node_c = Node(
-        id='b49ffa65-03e7-411d-be7a-6bbaf06543b9', 
-        data=NodeData(
-            technique_id=3,name='Low Mount', 
+        #id='b49ffa65-03e7-411d-be7a-6bbaf06543b9', 
+        id=3,
+        technique=Technique(
+            id=3, name='Low Mount',
+            description="Sitting directly on the opponent's stomach with knees close to their hips, providing a stable base and control but with limited submission opportunities.",
             tags=[{'id': 1, 'name': 'top'}],
             cat_id=1
         )
     )
     node_d = Node(
-        id='c0c64f73-a9a4-43f9-ad85-37b5a3b57cec', 
-        data=NodeData(
-            technique_id=41,name='Americana',
+        #id='c0c64f73-a9a4-43f9-ad85-37b5a3b57cec', 
+        id=4,
+        technique=Technique(
+            id=41, name='Americana',
+            description="Controlling the opponent from the side while pinning their shoulders and hips, maintaining pressure with chest-to-chest contact.",
             tags=[{'id': 11, 'name': 'shoulder'}],
             cat_id=2
         )
     )
     node_e = Node(
-        id='a766adaa-2f18-4285-9b02-94d02e71d63d', 
-        data=NodeData(
-            technique_id=39,name='Arm Bar', 
+        #id='a766adaa-2f18-4285-9b02-94d02e71d63d', 
+        id=5,
+        technique=Technique(
+            id=39, name='Arm Bar',
+            description="A joint lock that hyperextends the elbow joint, typically applied by trapping the opponent's arm between your legs and pulling on the wrist while pushing with the hips.",
             tags=[{'id': 12, 'name': 'elbow'}],
             cat_id=2
         )
     )
+    # Define constant edges based on sequence 581
+    edge_c_d = Edge(
+        id=1,
+        source_id=3, 
+        target_id=4,
+        note='Attempt Americana.'
+    )
+    edge_c_a = Edge(
+        id=2, 
+        source_id=3, 
+        target_id=1,
+        note='Opponent frames on hip, collect arm with knee, crossface, grab armpit, and throw leg over for triangle.'
+    )
+    edge_b_c = Edge(
+        id=3,
+        source_id=2, 
+        target_id=3,
+        note='Transition from side control to mount.'
+    )
+    edge_d_e = Edge(
+        id=4,
+        source_id=4, 
+        target_id=5,
+        note='Opponent defends Americana, transition to arm lock.'
+    )
+    # Pack raw nodes and edges into a list
+    rawNodes: list[Node] = [node_a, node_b, node_c, node_d, node_e]
+    rawEdges: list[Edge] = [edge_c_d, edge_c_a, edge_b_c, edge_d_e]
 
-    initialNodes: list[Node] = [node_a, node_b, node_c, node_d, node_e]
-    print(initialNodes)
-
-
-    # Setup edges
-
-
-
-
+    # reshape the constant nodes and edges
+    # generating unique ID's for passing back to the user 
+    idMap, reshapedNodes = shape_nodes(nodes=rawNodes)
+    reshapedEdges = shape_edges(idMap, rawEdges)
 
     # Initialize ReactFlow object
-    
+    # Pack response into ReactFlow model, ensuring type safety
+    # use the sequences name as the graph name
+    try:
+        jitsujournal = Reactflow(name='Sample/Test Sequence', 
+                                initialNodes=reshapedNodes, initialEdges=reshapedEdges)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_424_FAILED_DEPENDENCY,
+            detail=f'Failed to parse output into ReactFlow model. Error: {str(e)}'
+        )
 
-
-
-    return {"message": "Testing endpoint to pipe data into generateSolution"}
+    # Return jitsujournal friendly graph to the user
+    # FastAPI automatically dumps the model as JSON
+    return jitsujournal
 
 
 # Actual endpoint for processing a given user problem
