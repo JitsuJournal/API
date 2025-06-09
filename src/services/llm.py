e19f8eccfe7157b5b46cf94ab6d2c671e693d94d
harrisiva <harrishiv6@gmail.com>
2025-06-09 03:03:20 -0400

    Added the similarity search and grounding section with new grounding prompt thats a bit more updated/refined. Finally verified that flowcharts are being created properly with newly rich notes:

--- src/services/llm.py
+++ src/services/llm.py
@@ -21,7 +21,7 @@ def create_paragraph(client: genai.Client, problem: str):
     and generates a hypothetical answer in a paragraph as solution.
     """
     solution = client.models.generate_content(
-        model="gemini-2.5-flash-preview-05-20",
+        model="gemini-2.0-flash-lite",#"gemini-2.5-flash-preview-05-20",
         config=types.GenerateContentConfig(
             temperature=0.25
         ),
@@ -45,13 +45,12 @@ def create_embedding(client: genai.Client, paragraph: str):
     )
     return embedding
 
-def ground(client:genai.Client, problem:str, paragraphs: list[str], solution: str):
+def ground(client:genai.Client, problem:str, solution: str, similar: str):
     """
-    Given a list of paragraphs as strings, a users problem in jiu-jitsu,
-    and hypotehtical solution; ground said solution
-    to actually use the techniques, positions, and paths from the paragraphs.
-    Also refer to the paragraphs to remove contradictions and inconsistences
-    from the generated solution.
+    Given a user's problem, a hyde, and similar documents.
+    Ground the hyde to actually use the techniques, positions, 
+    and paths from the paragraphs. Remove contradictions 
+    and inconsistences or contradictions.
     """
     grounded = client.models.generate_content(
         model="gemini-2.5-flash-preview-05-20",
@@ -62,21 +61,21 @@ def ground(client:genai.Client, problem:str, paragraphs: list[str], solution: st
                 jiu-jitsu sequences and assessing if they solve a 
                 user problem.
             """,
-            response_mime_type="application/json",
-            response_schema=Solution,
             temperature=0.25
         ),
-        contents=[problem, paragraphs, solution,
+        contents=[problem, solution, similar,
             """
-            Given a problem faced by a jiu-jitsu practitioner, 
-            a list of paragraphs as potential solutions retrieved
-            from youtube tutorials, and a generated solution;
-            ground said solution to actually use the techniques, 
-            positions, and paths from the relevant paragraphs.
+            You are a expert in brazilian gi and no-gi jiu-jitsu and 
+            a professional coach. Given a problem faced by a 
+            jiu-jitsu practitioner, a generated solution,
+            and sequences as paragraphs from sequences retrived
+            from youtube, ground said solution to 
+            actually use the techniques, positions, 
+            and paths from the relevant paragraphs.
             
             Also refer to the paragraphs to remove any contradictions 
             or inconsistences from the generated solution. Ignore
-            paragraphs that don't actually solve the user problem.
+            paragraphs that don't actually solve or address the user problem.
             """]
     )
     return grounded
@@ -140,35 +139,52 @@ def create_flowchart(client: genai.Client, sequences: str, techniques: str):
     return flowchart
 
 if __name__=="__main__":
+    import json
+    from .db import conn_supabase, similarity_search, get_techniques
+
     client = conn_gemini()
     problem = "Simple ways to pass an oppoennts open and closed guard when i'm in top position and go into better positions to then go finish strong with submissions"
     solution = create_paragraph(client, problem)
     # TODO: Can try above with a more light weight model
     print(solution.text)
 
-    # extract each sequence in paragraph into steps w/ names
-    extracted: list[Sequence] = extract_sequences(client=client, paragraph=solution.text).parsed
+    # Initialize supabase client
+    supabase = conn_supabase()
+
+    # Create embedding to perform similarity search
+    embedding = create_embedding(client, paragraph=solution.text)
+    vector: list[float] = embedding.embeddings[0].values
+
+    # Retrive records similar to the generated solution from Supabase
+    # NOTE: Using default match threshold and count for searching
+    results = similarity_search(client=supabase, vector=vector)
+    # Flatten into a json string to pass to LLM for grounding
+    similar: str = json.dumps([{
+            'name': sequence['name'],
+            'paragraph': sequence['content']
+        } for sequence in results.data]
+    )
     
-    import json
+    # Ground the generated response using similar sequences
+    # from actual youtube tutorials and coaches
+    grounded = ground(client=client, problem=problem, 
+                      similar=similar,
+                      solution=solution.text)
+
+    # extract each sequence in grounded paragraph into steps w/ names
+    extracted: list[Sequence] = extract_sequences(client=client, paragraph=grounded.text).parsed
+
     # iterate over parsed sequences and dump into dict
     # for passing back into model as json string
     sequences: str = json.dumps([sequence.model_dump() for sequence in extracted])
 
-    from .db import conn_supabase, similarity_search, get_techniques
-    # Initialize supabase client
-    supaClient = conn_supabase()
-
-    # TODO: Find similar and ground
-
-
-    # Import techniques as json string
-    techniques = get_techniques(supaClient)
-
+    # import techniques as json string
+    techniques = get_techniques(supabase)
 
     # pass sequences and techniques to model
     # and create a flowchart without inconsistencies
     # or duplicates, which would be the APIs response
-    flowchart: Graph = create_flowchart(client, sequences, techniques)
+    flowchart: Graph = create_flowchart(client, sequences, techniques).parsed
 
 
     print(flowchart.model_dump_json(indent=2))
