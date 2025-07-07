5702d1d163619ec77f0904a0dde054f862b8d043
harrisiva <harrishiv6@gmail.com>
2025-07-07 18:00:11 -0400

    Drafted driver code and fetched/created video records for all videoIds upto index 101 on the unique id's list

--- src/utils/embed.py
+++ src/utils/embed.py
@@ -13,38 +13,54 @@
 
 
 if __name__=="__main__":
+    import time
+    from ..models.general import Video
     from ..services.db import conn_supabase
     from .. services.youtube import conn_youtube, get_basic_info
 
     # Connect to existing embeddings table
     # fetch all uniuqe video ID's stored
-    client = conn_supabase()
-    response = (
-        client.table("embeddings")
+    db_client = conn_supabase()
+    idFetchResponse = (
+        db_client.table("embeddings")
         .select("video_id")
         .execute()
     )
 
     # Length: 181
-    uniqueVideoIds = list(dict.fromkeys(d['video_id'] for d in response.data))
+    uniqueVideoIds = list(dict.fromkeys(d['video_id'] for d in idFetchResponse.data))
     print("Fetched videoId's to mem:", len(uniqueVideoIds))
 
-
     # Initialize client for using the youtube data API
-    client = conn_youtube()
+    yt_client = conn_youtube()
 
-    # Iterate over the video Id's and
-    # grab metadata using youtube data API
-    metadata = get_basic_info(client, uniqueVideoIds[0])
-    print(metadata)
+    for videoId in uniqueVideoIds[101:]:
+        print('Fetching basic info for:', videoId)
+        # Iterate over the video Id's and
+        # grab metadata using youtube data API
+        metadata: Video = get_basic_info(yt_client, videoId)
+        print('\tFetched metadata')
 
+        print('Creating records for', videoId)
+        # create records in videos table
+        # time.sleep to avoid any rate-limit errors
+        createRecordResponse = (
+            db_client.table('videos')
+            .insert({
+                'video_id': metadata.id,
+                'title': metadata.title,
+                'description': metadata.description,
+                'uploaded_at': metadata.uploaded_at,
+            })
+            .execute()
+        )
 
-    # create records in videos table
-    # time.sleep to avoid any rate-limit errors
+        print('\tcreated response:')
+        print(createRecordResponse)
+        print('--'*50)
 
-
-
-    # NOTE: Need to match sample data and contain any other metadata/info to Video model
+        time.sleep(5)
+        
     # NOTE/TODO: Make sure to set the embeddings videoId
     # as foreign key, making it easier to reference between
     # youtube videos and their sequence's embeddings.
