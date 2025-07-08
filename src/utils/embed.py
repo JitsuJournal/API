7eaffd54e169ef24bc55e9a9b3a5f6fed6b87e1f
harrisiva <harrishiv6@gmail.com>
2025-07-07 23:04:19 -0400

    Fixed bugs in the set_embedding_basic_info function and ensure it's working and handling the duplicate state by running. Need to comment out the function call and start writing the util script to pull recent log use for a given period, generate queries, search for informaiton, and create recordings. We also need to handle setting the videoId as a foreign key and finish case where we want to fetch the tutorials, breakdown sequences, and create embeddings when they're in the videos table and not in the embedding table

--- src/utils/embed.py
+++ src/utils/embed.py
@@ -6,14 +6,12 @@ from ..services.db import conn_supabase, get_unique_embedded_videoids, insert_vi
 from .. services.youtube import conn_youtube, get_basic_info
 # Third party
 
-def set_embedding_basic_info(timeout:int=5):
+def set_embedding_basic_info(start: int, timeout:int=5):
     """
     Function for collecting the basic information for all unique
     videos in the embedding table and storing that in the videos
     table. The function and data collection is used once as a script.
-
-    No error handling is in place at the moment. Safer to run function
-    in batches to avoid timeout errors regardless of the timeout value.
+    NOTE: Safer to run function in batches to avoid timeout errors.
     """
     # Initalize supabase connection to create db client
     # and fetch a list of unique video id's from the embedding table
@@ -23,17 +21,19 @@ def set_embedding_basic_info(timeout:int=5):
     yt_client = conn_youtube()
     # Iterate over the video Id's, grab their basic info
     # and insert records into the video table
-    # NOTE: No error handling at the moment to catch duplicates
-    # but enforcing unique values in the database layer
-    for id in uniqueIds: # NOTE: Run in batch to avoid timeout errors
+    for id in uniqueIds[start:]: # NOTE: Run in batch to avoid timeout errors
+        print(f'{id}: Fetching metadata')
         metadata: Video = get_basic_info(yt_client, id)        
         # Handle cases where no metadata is available 
         # by skipping the video and passing a msg to the terminal
         if metadata==None:
-            print(f'{id}: No metadata available. Skipped')
+            print(f'\tNo metadata available. Skipped')
             continue
         # Else create a record in the videos table
-        insert_video_record(metadata)
+        try:
+            insert_video_record(client=db_client, video=metadata)
+            print(f'\tInserted record to video table.')
+        except: print(f'\tFailed to insert video record to db.')
         # Call time.sleep to avoid rate-limit errors
         time.sleep(timeout)
     return
@@ -52,4 +52,7 @@ if __name__=="__main__":
     # youtube videos and their sequence's embeddings.
     # Maybe in the future we can expand embeddings with timestamps
     # and redirect users with timestamped urls.
-    pass
+    
+    # Call driver function to create records in video table
+    # for the unique video id's in the embedding table
+    set_embedding_basic_info(start=166, timeout=2)
